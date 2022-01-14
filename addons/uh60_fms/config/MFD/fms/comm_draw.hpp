f21d8ddc67c269b0581d4c7594c819d125f4b518
BroBeans <cpgbrobeans@gmail.com>
2022-01-14 20:34:09 +0800

    spacing?

--- addons/uh60_fms/config/MFD/fms/comm_draw.hpp
+++ addons/uh60_fms/config/MFD/fms/comm_draw.hpp
@@ -1,49 +1,49 @@
 TEXT_FMS_C(HEADER_TEXT,0.5,FMS_YHEAD)
-    source = "static";
-    text = "COMMUNICATIONS";
+  source = "static";
+  text = "COMMUNICATIONS";
 }; // HEADER_TEXT
 TEXT_FMS_L(RADIO_ONE_L1,FMS_MARGIN_L,FMS_Y1-FMS_HALF_LINE)
-    source = "static";
-    text = "COMM 1";
+  source = "static";
+  text = "VHF";
 }; // Radio1_Line1
 TEXT_FMS_L(RADIO_ONE_L2,FMS_MARGIN_L,FMS_Y1+FMS_HALF_LINE)
-    source = "static";
-    text = "133Mhz";
+  source = "static";
+  text = "188.666 MHz";
 }; // Radio1_Line2
 
 TEXT_FMS_L(RADIO_TWO_L1,FMS_MARGIN_L,FMS_Y2-FMS_HALF_LINE)
-    source = "static";
-    text = "COMM 2";
+  source = "static";
+  text = "UHF";
 }; // Radio2_Line1
 TEXT_FMS_L(RADIO_TWO_L2,FMS_MARGIN_L,FMS_Y2+FMS_HALF_LINE)
-    source = "static";
-    text = "135Mhz";
+  source = "static";
+  text = "247.000 MHz";
 }; // Radio2_Line2
 
 TEXT_FMS_L(RADIO_THREE_L1,FMS_MARGIN_L,FMS_Y3-FMS_HALF_LINE)
-    source = "static";
-    text = "COMM 3";
+  source = "static";
+  text = "FM";
 }; // Radio3_Line1
 TEXT_FMS_L(RADIO_THREE_L2,FMS_MARGIN_L,FMS_Y3+FMS_HALF_LINE)
-    source = "static";
-    text = "137Mhz";
+  source = "static";
+  text = "45.400 MHz";
 }; // Radio3_Line2
 
 TEXT_FMS_L(RADIO_FOUR_L1,FMS_MARGIN_L,FMS_Y4-FMS_HALF_LINE)
-    source = "static";
-    text = "COMM 4";
+  source = "static";
+  text = "FM";
 }; // Radio4_Line1
 TEXT_FMS_L(RADIO_FOUR_L2,FMS_MARGIN_L,FMS_Y4+FMS_HALF_LINE)
-    source = "static";
-    text = "139MHz";
-}; // Radio3_Line2
+  source = "static";
+  text = "41.800 MHz";
+}; // Radio4_Line2
 
 TEXT_FMS_R(SETTINGS_Text,FMS_MARGIN_R,FMS_Y5)
-    source = "static";
-    text = "OPT >";
+  source = "static";
+  text = "OPT >";
 }; // Menu_Text
 
 TEXT_FMS_L(MENU_Text,FMS_MARGIN_L,FMS_Y5)
-    source = "static";
-    text = "< MENU";
+  source = "static";
+  text = "< MENU";
 }; // Menu_Text
